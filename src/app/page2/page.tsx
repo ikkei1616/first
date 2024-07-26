74a869b9f50b522ed4b1b31a2fbf915079f6e6fc
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-07-26 15:07:37 +0900

    ページ2作生

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { disconnect } from "process";
+
+export default function Home() {
+  return (
+    <div className="title">
+      <p >page2</p>
+    </div>
+
+  );
+}
